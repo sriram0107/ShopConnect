89be2716a82705fdd5a83d183f919e91b23ba6df
Sriram Sathish <63444773+sriram0107@users.noreply.github.com>
2020-06-15 12:10:05 +0400

    Added event listener and axios post request
    To send automated messages

--- routes/stock.js
+++ routes/stock.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const fs = require("fs");
+const axios = require("axios");
 const router = express.Router();
+const EventEmitter = require("events");
+const eventEmitter = new EventEmitter();
 
 router.get("/", (req, res) => {
   if (req.session.login === true) {
@@ -15,12 +18,20 @@ router.post("/", (req, res) => {
   for (k in req.body) {
     t = k;
   }
-
   fs.readFile("user.json", "utf8", (err, file) => {
     var users = JSON.parse(file);
     users.forEach((mem) => {
       if (mem.username === req.session.user.username) {
         mem.stock[t] -= 1;
+        if (t == "mask" && mem.stock[t] == 999) {
+          eventEmitter.emit("mask", req.session.user);
+        } else if (t == "ppe" && mem.stock[t] == 999) {
+          eventEmitter.emit("ppe", req.session.user);
+        } else if (t == "ventilator" && mem.stock[t] == 49) {
+          eventEmitter.emit("ventilator", req.session.user);
+        } else if (t == "gown" && mem.stock[t] == 999) {
+          eventEmitter.emit("gown", req.session.user);
+        }
         req.session.user = mem;
       }
     });
@@ -32,4 +43,116 @@ router.post("/", (req, res) => {
   });
 });
 
+eventEmitter.on("mask", (user) => {
+  console.log("Entered mask");
+  console.log(user);
+  fs.readFile("shops.json", "utf8", (err, file) => {
+    if (err) console.log(err);
+    else {
+      var shops = JSON.parse(file);
+      /*var sfilt = shops.filter((mem) => {
+        user.address.includes(mem.city);
+      });
+      console.log(sfilt);*/
+      shops.forEach((mem) => {
+        if (mem.products.mask - 1000 >= -100) {
+          console.log("sending request.....");
+          axios
+            .post("http://localhost:5000/sendmessage", {
+              to: mem.name,
+              message: "We req masks ",
+              user: user,
+            })
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      });
+    }
+  });
+});
+eventEmitter.on("ppe", (user) => {
+  fs.readFile("shops.json", "utf8", (err, file) => {
+    if (err) console.log(err);
+    else {
+      var shops = JSON.parse(file);
+      var shops = shops.filter((mem) => {
+        user.address.includes(mem.city);
+      });
+      shops.forEach((mem) => {
+        if (mem.products.ppe - 1000 >= -100) {
+          axios
+            .post("http://localhost:5000/sendmessage", {
+              to: mem.name,
+              message: "We req ppe ",
+              user: user,
+            })
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      });
+    }
+  });
+});
+eventEmitter.on("ventilator", (user) => {
+  fs.readFile("shops.json", "utf8", (err, file) => {
+    if (err) console.log(err);
+    else {
+      var shops = JSON.parse(file);
+      var shops = shops.filter((mem) => {
+        user.address.includes(mem.city);
+      });
+      shops.forEach((mem) => {
+        if (mem.products.ventilator - 100 >= -10) {
+          axios
+            .post("http://localhost:5000/sendmessage", {
+              to: mem.name,
+              message: "We req ventilators ",
+              user: user,
+            })
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      });
+    }
+  });
+});
+eventEmitter.on("gown", (user) => {
+  fs.readFile("shops.json", "utf8", (err, file) => {
+    if (err) console.log(err);
+    else {
+      var shops = JSON.parse(file);
+      var shops = shops.filter((mem) => {
+        user.address.includes(mem.city);
+      });
+      shops.forEach((mem) => {
+        if (mem.products.gown - 2000 >= -100) {
+          axios
+            .post("http://localhost:5000/sendmessage", {
+              to: mem.name,
+              message: "We req gowns ",
+              user: user,
+            })
+            .then((res) => {
+              console.log(res);
+            })
+            .catch((err) => {
+              console.log(err);
+            });
+        }
+      });
+    }
+  });
+});
 module.exports = router;
